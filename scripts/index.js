bbb9378fe00db4945ce01dab8d29773cf4fef73c
ArtemVyzhimov <artemvyzhimov@gmail.com>
2025-01-17 10:39:38 +0300

    Sprint 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function createCard(data, handleDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  // Обработчик удаления карточки
+  deleteButton.addEventListener('click', () => handleDelete(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards(cards) {
+  cards.forEach((cardData) => {
+    const card = createCard(cardData, deleteCard);
+    placesList.append(card);
+  });
+}
+
+// Вызов рендера для начального массива карточек
+renderCards(initialCards);
