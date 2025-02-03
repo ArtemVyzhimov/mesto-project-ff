59888ca20b2eb49c730d447bcc74f7063045fd25
ArtemVyzhimov <artemvyzhimov@gmail.com>
2025-02-04 00:43:44 +0300

    3. Popup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,73 @@
+import '../pages/index.css'; 
+import { initialCards } from './cards.js';
+
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
+
+
+
+
+
+
+
+// Попапы
+const profileEditButton = document.querySelector('.profile__edit-button');
+const newCardButton = document.querySelector('.profile__add-button');
+const popups = document.querySelectorAll('.popup');
+const editPopup = document.querySelector('.popup_type_edit');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+const popupImage = imagePopup.querySelector('.popup__image');
+const popupCaption = imagePopup.querySelector('.popup__caption');
+
+// @todo: Функция открытия попапа
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', closeByEscape);
+}
+
+// @todo: Функция закрытия попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', closeByEscape);
+}
+
+// @todo: Закрытие по клавише Esc
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
+
+// @todo: Закрытие попапа кликом на оверлей и крестик
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+      closePopup(popup);
+    }
+  });
+});
+
+// @todo: Открытие попапов
+profileEditButton.addEventListener('click', () => openPopup(editPopup));
+newCardButton.addEventListener('click', () => openPopup(newCardPopup));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // @todo: Функция создания карточки
 function createCard(data, handleDelete) {
